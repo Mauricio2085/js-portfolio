54156b7813a4efb0f4d84d0856dab01ffc53718b
mauricio ocampo marulanda <maoca2085@gmail.com>
2023-06-11 20:50:17 -0500

    Webpack configuration complete

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,4 +1,4 @@
-const API = 'https://randomuser.me/api/';
+const API = process.env.API;
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
@@ -11,4 +11,4 @@ const getData = async (id) => {
   };
 };
 
-export default getData;
+export {getData};
